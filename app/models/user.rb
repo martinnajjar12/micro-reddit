b9c04823a28df6259f9e91777225179c4d074c7d
martinnajjar12 <martin_najjar@yahoo.com>
2020-11-02 23:48:09 +0300

    Add Comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
   validates :username, presence: true, uniqueness: true, length: { minimum: 5, maximum: 20 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email invalid"  }
   validates :password, presence: true
